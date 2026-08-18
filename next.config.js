6a90ddde0cebd6dc251e4dac5a2ef9cfa93802ad
Ruhvi2026 <ruhvi.main@gmail.com>
2026-08-19 04:15:22 +0530

    fix: resolve jwks-rsa jose ESM import issue by letting Next.js bundle jwks-rsa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['firebase-admin', 'jwks-rsa'],
+  serverExternalPackages: ['firebase-admin'],
   images: {
     remotePatterns: [
       {
